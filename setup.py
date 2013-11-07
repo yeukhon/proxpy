83ac6860c0390cc538829091fa2d67750f19852b
Yeuk Hon Wong <yeukhon@mozilla.com>
2013-11-07 17:26:56 -0500

    TASK: setup.py: Add proxpy.py as a console_script during pip install. Fixed #6.

--- setup.py
+++ setup.py
@@ -4,4 +4,8 @@ setup(name="proxpy",
     version="0.1",
     description="HTTP/HTTPS proxy server in Python.",
     packages=['proxpy'],
+    entry_points={
+        'console_scripts':
+            ['proxpy=proxpy.proxpy:main']
+    }
 )
